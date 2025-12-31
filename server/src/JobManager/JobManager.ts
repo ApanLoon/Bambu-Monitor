f84aa2d538d2c9335fb01fd2f2a638a8bf0b404b
ApanLoon <apanloon@live.co.uk>
2025-12-31 12:40:35 +0100

    Only request downloading of the project file when a new job is created.

--- server/src/JobManager/JobManager.ts
+++ server/src/JobManager/JobManager.ts
@@ -110,9 +110,10 @@ export class JobManager extends EventEmitter
                 this.CurrentJob.Name = status.subtask_name;
                 this.CurrentJob.GcodeName = status.gcode_file;
                 this.CurrentJob.State = JobState.Started;
+
+                setTimeout (() => this.emit (JobEvent.JobGetProject, this.CurrentJob), 2000); // Don't download the project immediately to give the printer time to store it fully.
             }
 
-            setTimeout (() => this.emit (JobEvent.JobGetProject, this.CurrentJob), 2000); // Don't download the project immediately to give the printer time to store it fully.
             this.emit (JobEvent.JobUpdated, this.CurrentJob);
         }
 
